abb36b73ba79a30f71e46477b134fa5370590fb9
tonalmathew <tonalmathew1999@gmail.com>
2020-04-09 11:59:59 +0530

    todo-v1

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let items = []; 
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    let today = new Date();
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+};
+
+let day = today.toLocaleDateString("en-US", options);
+
+res.render("list",{ kindOfDay: day, addItems: items});
+
+});
+
+
+app.post("/", function(req, res){
+
+    if(req.body.button === ""){
+        var item = req.body.newItem;
+        items.push(item);
+        res.redirect("/");
+    }else{
+        var del = req.body.deleteButton;
+        console.log(req.body.deleteButton);
+        console.log(del);
+        
+        for(i = 0; i<items.length;i++)
+        {
+            if(del === items[i]){
+                items.splice(i,1);
+                res.redirect("/");
+            }
+        }
+    }    
+}); 
+
+
+
+
+app.listen(5000, function(){
+    console.log("app listening on port 5000!");
+});
