88df1c55fe46c1d00b2b029cc42dc99c3dd6f9cf
tonalmathew <tonalmathew1999@gmail.com>
2020-04-13 11:56:30 +0530

    added database

--- app.js
+++ app.js
@@ -1,14 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
 const app = express();
 
-let items = []; 
+
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true, useUnifiedTopology: true});
+
+const itemsSchema = {
+    name: String
+}; 
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+    name: "Welcome to your todolist!"
+});
+
+const item2 = new Item({
+    name: "Hit the + button to add an item"
+});
+
+const item3 = new Item({
+    name: "Hover over the right side of the list and hit the delete button to delete an item"
+});
+
+const defaultItems = [item1, item2, item3];
+
+
 
 app.get("/", function(req, res){
     let today = new Date();
@@ -20,7 +45,24 @@ app.get("/", function(req, res){
 
 let day = today.toLocaleDateString("en-US", options);
 
-res.render("list",{ kindOfDay: day, addItems: items});
+Item.find({}, function(err, foundItems){
+
+    if(foundItems.length === 0){
+        Item.insertMany(defaultItems, function(err){
+            if(err){
+                console.log(err);
+            }else{
+                console.log("Insertion sucessfull!");
+            }
+        });
+        res.redirect("/");
+    }else{
+        res.render("list",{ kindOfDay: day, addItems: foundItems});
+    }
+    
+})
+
+
 
 });
 
@@ -28,18 +70,21 @@ res.render("list",{ kindOfDay: day, addItems: items});
 app.post("/", function(req, res){
 
     if(req.body.button === ""){
-        var item = req.body.newItem;
-        items.push(item);
+        const itemName = req.body.newItem;
+        const item = new Item({
+            name: itemName
+        });
+        item.save();
         res.redirect("/");
     }else{
         var del = req.body.deleteButton;
-        for(i = 0; i<items.length;i++)
-        {
-            if(del === items[i]){
-                items.splice(i,1);
-                res.redirect("/");
+        
+        Item.findByIdAndRemove(del, function(err){
+            if(!err){
+                console.log("Sucessfully deleted!")
             }
-        }
+            res.redirect("/");
+        });
     }    
 });
 
